1bd11d0d128384a013e6138ebea0d23486d1fae8
Chongshi Tam <339136347@qq.com>
2014-10-17 01:57:38 +0800

    增加信息备注

--- src/just5.js
+++ src/just5.js
@@ -1,3 +1,15 @@
+/*!
+ * just5 JavaScript Library v@1.0
+ * http://cocoscript.com/
+
+ * author: Chongshi Tam
+ *
+ * Copyright 2014 just5 Foundation
+ * Released under the MIT license
+ * https://github.com/tanchongshi/just5/blob/master/LICENSE
+ *
+ * Date: @20141017
+ */
 define([
   './core'
 ], function (just5) {
